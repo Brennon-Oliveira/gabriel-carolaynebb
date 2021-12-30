f866dac6d72243a8ccacdd40489d8236f45b87f3
Brennon Gabriel de Oliveira <55529693+Brennon-Oliveira@users.noreply.github.com>
2021-12-29 23:35:07 -0300

    Iniciando sessão do quiz

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header.component";
 import Banner from "./sections/Banner/Banner";
 import Songs from "./sections/Songs/Songs";
+import Quiz from "./sections/Quiz/Quiz";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Header />
             <Banner />
             <Songs />
+            <Quiz />
         </>
     );
 }
---
+++ src/screens/Home/sections/Quiz/Quiz.tsx
@@ -0,0 +1,11 @@
+import Container from "./../../../../components/Container/Container.component";
+import Title from "./../../../../components/Title/Title";
+export default function Quiz() {
+    return (
+        <section id="Quiz">
+            <Container>
+                <Title>Quiz</Title>
+            </Container>
+        </section>
+    );
+}
